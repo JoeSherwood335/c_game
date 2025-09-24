6426ae00eef068e0b4d047fba68d9625908c1f30
Joe Sherwood <joesherwood335@live.com>
2025-09-24 17:31:43 -0400

    bug fixes

--- c_libraries/gameCore/src/gameCore.c
+++ c_libraries/gameCore/src/gameCore.c
@@ -16,6 +16,9 @@ int gc_error_code = 0; // Initialize error code
 
 void gc_init_game_contexts()
 {
+
+    setTag("Core");
+
     for (int i = 0; i < MAX_PLAYERS; i++) {
         g_player_context[i] = NULL;
     }
@@ -26,16 +29,22 @@ void gc_init_game_contexts()
         g_item_context[i] = NULL;
     }
 
-    u_Log_Debug("GameCore: Game contexts initialized.\n");
-    //g_error_code = 0;
+    u_Log_Debug("Game contexts initialized.\n");
+    
+    setTag("");
 }
+
+// add functions
 
 g_char_descriper gc_add_Character_to_context(g_character *player)
 { 
+      
+    u_Log_Verbose("gc_add_Character_to_context %s \n", player->name);
+
     if (player_count >= MAX_PLAYERS) {
       
-      u_Log_Error("GameCore: Failed to add player to context. Maximum players reached.\n");
-      gc_error_code = ERROR_MAX_PLAYERS_REACHED; // Set error code for maximum players reached
+      u_Log_Error("Failed to add player to context. Maximum players reached.\n");
+      
       return -1; // Failed to add player
     }
 
@@ -43,12 +52,13 @@ g_char_descriper gc_add_Character_to_context(g_character *player)
         if (g_player_context[i] == NULL) {
             g_player_context[i] = player;
             player_count++;
-            u_Log_Debug("GameCore: Player added to context at index %i. \n", i);
+            u_Log_Debug("Player added to context at index %i. \n", i);
+    
             return i; // Player added successfully
         }
     }
     
-    u_Log_Error("GameCore: Failed to add player to context. Maximum players reached.\n");
+    u_Log_Error("Failed to add player to context. Maximum players reached.\n");
     gc_error_code =  ERROR_MAX_PLAYERS_REACHED;
     return -1; // Failed to add player
 }
@@ -63,25 +73,27 @@ g_room_descriper gc_add_Room_to_context(g_room *room)
         if (g_room_context[i] == NULL) {
             g_room_context[i] = room;
             room_count++;
-            u_Log_Debug("GameCore: Room added to context at index %i.\n", i);
+            u_Log_Debug("Room added to context at index %i.\n", i);
             return i; // Room added successfully
         }
     }
-
+  
     return -1; // Failed to add room
 }  
+
+// get functions
 
 g_character *gc_get_Character_from_context(g_char_descriper char_fd)
 {
     if (char_fd < 0 || char_fd >= MAX_PLAYERS) {
 
-        gc_error_code = 3; // Set error code for invalid player index
+        u_Log_Error("Item Index outside of range %i \n", char_fd);
 
         return NULL; // Invalid player index
     }
 
     if (g_player_context[char_fd] == NULL) {
-        u_Log_Error("GameCore: No player found at index %i.\n", char_fd);
+        u_Log_Error("o player found at index %i.\n", char_fd);
         return NULL; // No player found at this index
     }
     return g_player_context[char_fd];
@@ -90,7 +102,10 @@ g_character *gc_get_Character_from_context(g_char_descriper char_fd)
 g_room *gc_get_Room_from_context(g_char_descriper room_fd)
 {
     if (room_fd < 0 || room_fd >= MAX_ROOMS) {
-        return NULL; // Invalid room index
+
+      u_Log_Error("Item Index outside of range %i \n", room_fd);
+
+      return NULL; // Invalid room index
     }
     return g_room_context[room_fd];
 }
@@ -98,11 +113,15 @@ g_room *gc_get_Room_from_context(g_char_descriper room_fd)
 g_item *gc_get_Item_from_context(g_char_descriper item_fd)
 {
     if (item_fd < 0 || item_fd >= MAX_ITEMS) {
-        gc_error_code = ERROR_INVALID_ITEM_INDEX; // Set error code for invalid item index
+        
+        u_Log_Error("Item Index outside of range %i \n", item_fd);
+
         return NULL; // Invalid item index
     }
     return g_item_context[item_fd];
 }
+
+// remove functions
 
 void gc_remove_Room_from_context(g_char_descriper room_fd)
 {
@@ -114,7 +133,7 @@ void gc_remove_Room_from_context(g_char_descriper room_fd)
     if (g_room_context[room_fd] != NULL) {
         g_room_context[room_fd] = NULL;
         free(g_room_context[room_fd]);
-        u_Log_Debug("GameCore: Room removed from context at index %i.\n", room_fd);
+        u_Log_Debug("Room removed from context at index %i.\n", room_fd);
         room_count--;
     }
 }
@@ -125,7 +144,7 @@ void gc_remove_Character_from_context(g_char_descriper char_fd)
     u_Log_Debug("Destroying char_fd i \n", char_fd);
 
     if (char_fd < 0 || char_fd >= MAX_PLAYERS) {
-        u_Log_Error("GameCore: Invalid player index %d.\n", char_fd);
+        u_Log_Error("Invalid player index %d.\n", char_fd);
               
         gc_error_code = ERROR_INVALID_PLAYER_INDEX; // Set error code for invalid player index
         return; // Invalid player index
@@ -137,7 +156,7 @@ void gc_remove_Character_from_context(g_char_descriper char_fd)
         player_count--;
     }
 
-    u_Log_Debug("GameCore: Player removed from context at index %i.\n", char_fd);
+    u_Log_Debug("Player removed from context at index %i. \n", char_fd);
 }
 
 void gc_remove_Item_from_context(g_char_descriper item_fd)
@@ -154,17 +173,20 @@ void gc_remove_Item_from_context(g_char_descriper item_fd)
         item_count--;
     }
 
-    u_Log_Debug("GameCore: Item removed from context at index %d.\n", item_fd);
+    u_Log_Verbose("Item removed from context at index %d. \n", item_fd);
 }
 
 void gc_destroy_game_contexts()
 {
+    u_Log_Verbose("destroy_game_contexts Started \n");
+
     for (int i = 0; i < MAX_PLAYERS; i++) {
         if (g_player_context[i] != NULL) {
            
             gc_remove_Character_from_context(i); // Use the remove function to free memory
         }
-    }
+      }
+    
     for (int i = 0; i < MAX_ROOMS; i++) {
         if (g_room_context[i] != NULL) {
             
@@ -178,7 +200,7 @@ void gc_destroy_game_contexts()
         }
     }
 
-    u_Log_Verbose("GameCore: Game contexts destroyed.\n");
+    u_Log_Verbose("Game contexts destroyed. \n");
     player_count = 0;
     room_count = 0;
     item_count = 0;
