9cfe2466cda49fd308fb3ecbe30cad2c742d6030
Joe Sherwood <joesherwood335@live.com>
2025-10-24 21:59:46 -0400

    expand game

--- c_libraries/gameVars/src/gameVars.h
+++ c_libraries/gameVars/src/gameVars.h
@@ -18,35 +18,56 @@ extern const int DOWN;
 
 extern int g_error_code;
 
-enum g_header {
-    G_HEADER_HANDSHAKE = 0,
-    G_HEADER_MOVE = 1,
-    G_HEADER_MESSAGE = 2,
-    G_HEADER_EXIT = 3,
-    G_HEADER_ROOM_INFO = 4,
-    G_HEADER_PLAYER_INFO = 5,
-    G_HEADER_ERROR = -1 
-};
+typedef enum  {
+    G_FOCUS_ROOM = 0,
+    G_FOCUS_ITEM = 1,
+    G_FOCUS_CHARACTER = 2
+} g_focus_type; 
+
+typedef enum {
+    G_ACTION_MOVE = 0,
+    G_ACTION_TASK = 1,
+    G_ACTION_GESTURE = 2,
+    G_ACTION_EMOTE_TALK = 4,
+    G_ACTION_ATTACK = 5
+} g_action_type;
+
+typedef enum {
+    G_CHARACTER_AGGRESSIVE = 0,
+    G_CHARACTER_PASSIVE = 1,
+    G_CHARACTER_NEUTRAL = 2
+} g_character_state;
 
 typedef struct {
     int id;
     char name[50];
     void *current_room;
+    void *focus_object;
+    g_focus_type focus_type;
+    g_item *left_hand;
+    g_item *right_hand;
+    g_character_state state;
+    int aggression_score; // Scale of 0-100 likely to attack
 } g_character;
+
 
 typedef struct {
     int id;
+    char name[11];
     char description[100];
     g_character *players[10]; 
     void *directions[6]; // 6 directions: N, S, E, W, U, D
     void *next_room; // Pointer to the next room
+    g_focus_type focus_type;
+    
 } g_room;
 
 typedef struct {
     int id;
     char name[50];
     char description[100];
-} g_item;
+    g_focus_type focus_type;
+} g_item; // Basic item structure
 
 typedef struct {
     void *source;
